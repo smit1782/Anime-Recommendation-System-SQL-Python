cc5e5eab3cdda2cac967134ed43f631288387d42
Patel Smit D <78785592+smit1782@users.noreply.github.com>
2024-02-05 23:57:26 -0500

    Add files via upload

---
+++ TestScript.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+# Run with: ./TestScript.sh | python3 animeClient.py
+
+# Test case 0
+# "Attempting Command 0: "
+echo "0"
+
+# Test case 1
+# "Attempting Command 1: "
+echo "1"
+echo "Action"
+echo "1"
+echo "Magic"
+echo "1"
+echo "Fantasy"
+echo "1"
+echo "Adventure"
+
+# Test case 2
+# "Attempting Command 2: "
+echo "2"
+echo "1"
+echo "5.55"
+echo "1"
+echo "2"
+echo "10"
+echo "6.55"
+echo "0"
+
+
+# Test case 3
+# "Attempting Command 3: "
+echo "3"
+echo "100"
+echo "6.8"
+echo "0"
+
+# Test case 4
+# "Attempting Command 4: "
+echo "4"
+echo "1"
+
+# Test case 5
+# "Attempting Command 5: "
+echo "5"
+echo "30"
+
+# Test case 6
+# "Attempting Command 6: "
+echo "6"
+echo "Neon Genesis Evangelion"
+echo "6"
+echo "Naruto"
+echo "6"
+echo "One Piece"
+echo "6"
+echo "Dragon"
+
+# Test case 7
+# "Attempting Command 7: "
+echo "7"
+echo "PG-13"
+echo "magic"
+echo "7"
+echo "R+"
+echo "Action"
+
+# Test case 8
+# "Attempting Command 8: "
+echo "8"
+echo "R+"
+
+# Test case 9
+# "Attempting Command 9 [credits]: "
+echo "9"
+
+# Test case -1
+# "Attempting Command -1 [exit]: "
+echo "-1"
